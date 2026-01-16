3da7ac14d4f91761adbbdbad6aae4655b004f95a
Bunshaman <benschurman456@gmail.com>
2026-01-15 22:14:36 -0600

    Fixed crashes when creating blueprints with quality heat pipes Upgrade planners won't display multiple heat pipes

--- control.lua
+++ control.lua
@@ -1,46 +1,49 @@
-local on_built = function (data)
-    local entity = data.entity --[[@as LuaEntity]]
-    if entity.quality.level == 0 then return end
-    local surface = entity.surface
+--- Updates an invidiual heat pipe to be a quality heat pipe.
+--- @param pipe LuaEntity
+--- @param surface LuaSurface
+--- @param quality LuaQualityPrototype
+local function pipe_swap(pipe, surface, quality)
+    if (pipe.valid == false) or (surface.valid == false) or (quality.valid == false) then return end
+    if quality.level == 0 then return end
+    if string.find(pipe.name, "QHP-") then log("Double QHP detected. Report to mod author with information to replicate if possible.") return end
     local info = {
-        name = entity.quality.name.."-"..entity.name,
-        position = entity.position,
-        quality = entity.quality,
-        force = entity.force,
+        name = "QHP-"..quality.name.."-"..pipe.name,
+        position = pipe.position,
+        quality = pipe.quality,
+        force = pipe.force,
         fast_replace = true,
-        player = entity.last_user,
-        temperature = entity.temperature
+        player = pipe.last_user,
     }
-    entity.destroy()
-    surface.create_entity(info)
+
+    local temp = pipe.temperature
+    local new_pipe = surface.create_entity(info)
+    new_pipe.temperature = temp
+    pipe.destroy()
 end
 
-local rescan = function ()
+
+local function rescan()
     for _, surface in pairs(game.surfaces) do
         for _, heat_pipe in pairs(surface.find_entities_filtered{name="heat-pipe"}) do
             if heat_pipe.quality.level == 0 then goto continue end
-            local info = {
-                name = heat_pipe.quality.name.."-heat-pipe",
-                position = heat_pipe.position,
-                quality = heat_pipe.quality,
-                fast_replace = true,
-                force = heat_pipe.force,
-                player = heat_pipe.last_user,
-                temperature = heat_pipe.temperature
-            }
-            heat_pipe.destroy()
-            surface.create_entity(info)
+            pipe_swap(heat_pipe, surface, heat_pipe.quality)
             ::continue::
         end
     end
+end
+
+
+local function on_built(event)
+    local entity = event.entity         --[[@as LuaEntity]]
+    local surface = entity.surface      --[[@as LuaSurface]]
+    local quality = entity.quality      --[[@as LuaQualityPrototype]]
+    pipe_swap(entity, surface, quality)
 end
 
 local heat_pipe_filter = {
     filter = "type",
     type = "heat-pipe"
 }
-
-
 
 script.on_init(rescan)
 
@@ -49,3 +52,4 @@ script.on_event(defines.events.on_space_platform_built_entity,  on_built, {heat_
 script.on_event(defines.events.script_raised_built,             on_built, {heat_pipe_filter})
 script.on_event(defines.events.script_raised_revive,            on_built, {heat_pipe_filter})
 script.on_event(defines.events.on_robot_built_entity,           on_built, {heat_pipe_filter})
+script.on_event(defines.events.on_entity_cloned,                on_built, {heat_pipe_filter})
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,10 +1,13 @@
-local blacklist = {["quality-unknown"] = true}
+local quality_blacklist = {["quality-unknown"] = true}
+
+
 local quality_names = {}
 for quality_name, quality_prototype in pairs(data.raw["quality"]) do
-	if not blacklist[quality_name] then
+	if not quality_blacklist[quality_name] then
 		quality_names[quality_name] = quality_prototype
 	end
 end
+
 
 local heat_pipes = {}
 for _, quality_prototype in pairs(quality_names) do
@@ -16,21 +19,33 @@ for _, quality_prototype in pairs(quality_names) do
 		if quality_prototype.name == "normal" then
 		else
 			heat_pipe.localised_name = {"entity-name."..heat_pipe.name}
-			heat_pipe.name = k.."-"..heat_pipe.name
-			heat_pipe.heating_radius = heating_radius * (1 + quality_prototype.level)	--- old: heating_radius + q.level
+			heat_pipe.name = "QHP-"..k.."-"..heat_pipe.name
+
+			-- hacky code to prevent spam of heat pipes in upgrade planners because different collision_boxes will not show up as proper upgrades for the source in placeable_by
+			if quality_prototype.level % 4 == 0 then	
+				heat_pipe.collision_box[1][1] = heat_pipe.collision_box[1][1] - quality_prototype.level/1000
+			elseif quality_prototype.level % 4 == 1 then
+				heat_pipe.collision_box[1][2] = heat_pipe.collision_box[1][2] - quality_prototype.level/1000
+			elseif quality_prototype.level % 4 == 2 then
+				heat_pipe.collision_box[2][1] = heat_pipe.collision_box[2][1] + quality_prototype.level/1000
+			elseif quality_prototype.level % 4 == 3 then
+				heat_pipe.collision_box[2][2] = heat_pipe.collision_box[2][2] + quality_prototype.level/1000
+			end
+			heat_pipe.heating_radius = heating_radius * (1 + quality_prototype.level)
 			if data.raw.item[heat_pipe_name] then
 				heat_pipe.placeable_by = { item = heat_pipe_name, count = 1, quality = k }
 			else
-				log("No item found for heat pipe: " .. heat_pipe_name..". Proceeding without the placeable_by tag.")
+				--log("No item found for heat pipe: " .. heat_pipe_name..". Proceeding without the placeable_by tag.")
 			end
 
+			heat_pipe.hidden = true
 			heat_pipe.hidden_in_factoriopedia = true
 
 			if heat_pipe.max_health then heat_pipe.max_health = heat_pipe.max_health * v end
-			if heat_pipe.next_upgrade then heat_pipe.next_upgrade = k.."-"..heat_pipe.next_upgrade end
+			if heat_pipe.next_upgrade then heat_pipe.next_upgrade = "QHP-"..k.."-"..heat_pipe.next_upgrade end
 			table.insert(heat_pipes, heat_pipe)
 		end
 	end
 end
-data:extend(heat_pipes)
 
+data:extend(heat_pipes)
